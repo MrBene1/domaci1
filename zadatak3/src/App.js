dad85625492ab01be4cec473d088753310dabfc1
Obren <femicobren@gmail.com>
2022-05-17 20:14:10 +0200

    Added home and add post pages

--- zadatak3/src/App.js
+++ zadatak3/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import './App.css';
+import Home from "./pages/Home.js";
+import AddPost from "./pages/AddPost.js";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 class App extends React.Component {
   render(){
     return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/add-post" element={<AddPost/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
     );
   }
---
+++ zadatak3/src/pages/AddPost.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AddPost() {
+    return ( <div>Add post</div> );
+}
+
+export default AddPost;
