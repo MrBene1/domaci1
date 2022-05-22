ce6af56f9f0433b8e9c52a89aa2002fe4dffb52c
Obren <femicobren@gmail.com>
2022-05-23 00:13:35 +0200

    Listed blogs on home page

--- zadatak3/src/pages/AddPost.js
+++ zadatak3/src/pages/AddPost.js
@@ -1,18 +1,12 @@
 import React, { useState } from 'react';
 import '../App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function AddPost() {
     const [title, setTitle] = useState("");
     const [img, setImg] = useState("");
     const [author, setAuthor] = useState("");
     const [content, setContent] = useState("");
-
-    /*const handleSubmit = e => {
-        e.preventDefault();
-        setTitle("");
-        setDescription("");
-        setAuthor("Marko");
-      };*/
 
     const inputTitle = e => {
         setTitle( e.target.value );
@@ -32,17 +26,20 @@ function AddPost() {
     console.log(author);
     console.log(content);
     return ( 
-    <div className='add-post'>
-        <div className='main'>
-            <form>
-                <label className='label'>Dodaj post</label>
-                <input placeholder="Naziv" name="title" type="text" className="input" onChange={inputTitle}/>
-                <input placeholder="Url slike" name="img" type="text" className="input" onChange={inputImg}/>
-                <input placeholder="Ime autora" name="autor" type="text" className="input" onChange={inputAuthor}/>
-                <input placeholder="Sadrzaj" name="content" type="textarea" className="input" onChange={inputContent}/>
-                <input type="submit" value="Dodaj post" className="submit"></input>
-            </form>
+    <div>
+        <div className='add-post'>
+            <div className='main'>
+                <form>
+                    <label className='label'>Dodaj post</label>
+                    <input placeholder="Naziv" name="title" type="text" className="input" maxLength="20" onChange={inputTitle}/>
+                    <input placeholder="Url slike" name="img" type="text" className="input" onChange={inputImg}/>
+                    <input placeholder="Ime autora" name="autor" type="text" className="input" maxLength="20" onChange={inputAuthor}/>
+                    <textarea className='content' name="content" maxLength="250" placeholder='Sadrzaj' onChange={inputContent}></textarea>
+                    <input type="submit" value="Dodaj post" className="submit"></input>
+                </form>
+            </div>
         </div>
+        <Link to="/" className='back-button'>Nazad</Link>
     </div> );
 }
 
--- zadatak3/src/pages/Home.js
+++ zadatak3/src/pages/Home.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '../App.css';
 
 function Home() {
+
+    
+    const [data,setData]=useState([]);
+    const getData=()=>{
+        fetch('https://jsonblob.com/api/jsonBlob/976178308425465856')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setData(data)
+        });
+    }
+    useEffect(()=>{
+        getData()
+    },[])
+    
     return ( 
     <div className='App'>
-        <h2>Svi postovi</h2>
+        <h1>Svi postovi</h1>
+        <div className='home-grid'>
+            {data.map(blog => (<div key={blog.id} className='blog-item'>
+                <img src={blog.img} alt='blog'/>
+                <h3>{blog.title}</h3>
+                <p>{blog.content}</p>
+            </div>))}
+        </div>
     </div> );
 }
 
